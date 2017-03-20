e6b7f3917aeb6688281c1269fdb0c32068e6f168
victor <victor_riojas@yahoo.com.mx>
2017-03-19 22:59:14 -0600

    Initial Commit

---
+++ Favoritos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Favoritos
+//
+//  Created by Victor Hugo Vázquez Riojas on 3/19/17.
+//  Copyright © 2017 Victor Hugo Vázquez Riojas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
